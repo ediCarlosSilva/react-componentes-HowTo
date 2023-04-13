5098729ca9c0c416a9f3ff9fec054e4054d8a793
Edi Carlos <edi_csilva@hotmail.com>
2023-04-13 19:09:04 -0300

    adicionado função de callback que atualiza a cor de fundo

--- src/App.js
+++ src/App.js
@@ -197,6 +197,15 @@ function App() {
     console.log('Deletando Colaborador.');
   }
 
+  function mudarCorDoTime(cor, nome) {
+    setTimes(times.map(time => {
+      if (time.nome === nome) {
+        time.corSecundaria = cor;
+      }
+      return time;
+    }))
+  }
+
   return (
     <div>
       <Banner />
@@ -205,6 +214,7 @@ function App() {
         <h1>Minha organização</h1>
         {times.map((time, indice) =>
           <Time
+            mudarCor={mudarCorDoTime}
             key={indice}
             time={time}
             colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
